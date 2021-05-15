15264cd7ae856e6d1b152ab0d03366f712a38929
hikoand <71857799+hikoand@users.noreply.github.com>
2021-05-16 00:59:58 +0900

    count-ani

--- js/menu.js
+++ js/menu.js
@@ -30,24 +30,60 @@ $(function () {
   });
 });
 
+// $(function () {
+//   let countHeightObj = $('.countNum').offset();
+//   let counterHeight = countHeightObj[Object.keys(countHeightObj)[0]];
+//   let f = true;
+
+//   $(window).scroll(function () {
+//     let scTop = $(this).scrollTop();
+//     // console.log(scTop);
+//     // console.log(counterHeight);
+//   });
+
+//   scTop = $(this).scrollTop();
+//   // console.log(scTop);
+
+//   if (scTop > counterHeight) {
+//     console.log('oka');
+//     if (f == true) {
+//       // 1. each(): 복수의 .count를 각각 받아온다.
+//       $('.countNum').each(function () {
+//         // 2. prop(), Counter라는 속성을 생성한 뒤 0으로 초기화 한다.
+//         $(this)
+//           .prop('Counter', 0)
+//           // 3. animate(), 각각의 text를 받아와 Counter에 넣어 애니메이트한다.
+//           .animate(
+//             {
+//               Counter: $(this).text(),
+//             },
+//             // 4. 부드러운 애니메이션을 만들기 위해 속성을 지정한다.
+//             {
+//               duration: 3000,
+//               easing: 'swing',
+//               // 5. step(), 애니메이션 과정을 받아온다.
+//               step: function (now) {
+//                 // 6. 소숫점을 올려서 반환된 정수를 표시한다.
+//                 $(this).text(Math.ceil(now));
+//                 f = false;
+//               },
+//             }
+//           );
+//       });
+//     }
+//   }
+// });
+
 $(function () {
-  let countHeightObj = $('.countNum').offset();
-  let counterHeight = countHeightObj[Object.keys(countHeightObj)[0]];
-  let f = true;
-
+  let a = 0;
   $(window).scroll(function () {
-    let scTop = $(this).scrollTop();
-    // console.log(scTop);
-    // console.log(counterHeight);
-  });
+    console.log(`elem offsetTop : ${$('.countNum').offset().top}`);
+    console.log(`window heihgt : ${window.innerHeight}`);
 
-  scTop = $(this).scrollTop();
-  // console.log(scTop);
+    let oTop = $('.countNum').offset().top - window.innerHeight;
+    console.log(oTop);
 
-  if (scTop > counterHeight) {
-    console.log('oka');
-    if (f == true) {
-      // 1. each(): 복수의 .count를 각각 받아온다.
+    if (a == 0 && $(window).scrollTop() > oTop) {
       $('.countNum').each(function () {
         // 2. prop(), Counter라는 속성을 생성한 뒤 0으로 초기화 한다.
         $(this)
@@ -65,11 +101,11 @@ $(function () {
               step: function (now) {
                 // 6. 소숫점을 올려서 반환된 정수를 표시한다.
                 $(this).text(Math.ceil(now));
-                f = false;
+                a = 1;
               },
             }
           );
       });
     }
-  }
+  });
 });
