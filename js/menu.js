802564111ba412962314a64ac5579bb9f683e4fc
hikoand <71857799+hikoand@users.noreply.github.com>
2021-05-17 23:04:01 +0900

    CSS: media query

--- js/menu.js
+++ js/menu.js
@@ -85,21 +85,16 @@ $(function () {
 
     if (a == 0 && $(window).scrollTop() > oTop) {
       $('.countNum').each(function () {
-        // 2. prop(), Counter라는 속성을 생성한 뒤 0으로 초기화 한다.
         $(this)
           .prop('Counter', 0)
-          // 3. animate(), 각각의 text를 받아와 Counter에 넣어 애니메이트한다.
           .animate(
             {
               Counter: $(this).text(),
             },
-            // 4. 부드러운 애니메이션을 만들기 위해 속성을 지정한다.
             {
               duration: 3000,
               easing: 'swing',
-              // 5. step(), 애니메이션 과정을 받아온다.
               step: function (now) {
-                // 6. 소숫점을 올려서 반환된 정수를 표시한다.
                 $(this).text(Math.ceil(now));
                 a = 1;
               },
